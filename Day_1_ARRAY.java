75ff7171dc0136ff2d4d31b522f75d13ee6bc9c5
Keerthana J <98206044+itzz-keerthi@users.noreply.github.com>
2023-10-03 23:06:10 +0530

    Count number of Good Pairs

---
+++ Day_1_ARRAY.java
@@ -0,0 +1,46 @@
+/*
+1512. Number of Good Pairs
+
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+
+*/
+//brute force approach
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if((i<j) && (nums[i]==nums[j])){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
