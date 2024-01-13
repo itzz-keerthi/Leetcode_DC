34309b90b655f954e2b35a70e5cf730d9d6206ec
Keerthana J <98206044+itzz-keerthi@users.noreply.github.com>
2024-01-13 23:01:50 +0530

    interesting concept

---
+++ 237_optimal.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
+}
+
+/*
+TC: O(1)
+SC: O(1)
+*/
