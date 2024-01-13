3e30d454865285a7c4bb4a60e8478d28a76cb71c
Keerthana J <98206044+itzz-keerthi@users.noreply.github.com>
2024-01-13 22:57:36 +0530

    skipping unncessary steps

---
+++ 1534_slightoptimal.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        //brute force
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                //avoiding unnecessary iterations
+                if(Math.abs(arr[i]-arr[j])>a) continue;
+                for(int k=j+1;k<arr.length;k++){
+                    if((Math.abs(arr[j]-arr[k])<=b)&&(Math.abs(arr[i]-arr[k])<=c)){
+                    count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
